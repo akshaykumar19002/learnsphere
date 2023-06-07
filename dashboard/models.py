22ac4fc611bfc725ba6118cb9f94bc8028010ebb
rachurisangamesh <50862212+rachurisangamesh@users.noreply.github.com>
2023-06-06 22:01:50 -0400

    Add files via upload

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    course_id = models.CharField(max_length=50, unique=True)
+    course_name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    duration = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.course_name
